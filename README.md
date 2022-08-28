9b702e84f3e698236a1ed59a66982f8187faf6ce
Alex Filenko <aleksey.filenko@careerist.com>
2022-08-28 16:21:42 +0300

    Init project

---
+++ README.md
@@ -0,0 +1,6 @@
+# Alien Invasion
+## To start:
+1. python -m venv venv
+2. bash: source venv/Scripts/activate, windows: venv\Scripts\activate
+3. pip install -r requirements.txt
+4. python alien_invasion.py
---
+++ alien_invasion.py
@@ -0,0 +1,52 @@
+import sys
+import os.path
+import pygame
+
+from settings import Settings
+from src.models.ship import Ship
+import src.utils.game_functions as gf
+from pygame.sprite import Group
+from src.models.game_stats import GameStats
+from src.models.button import Button
+from src.models.scoreboard import Scoreboard
+import json
+
+filename = 'score.json'
+
+def run_game():
+    #Инициализация данных pygame
+    pygame.init()
+    #Создание экрана 
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_widht, ai_settings.screen_height))
+    #Создание кнопки
+    play_button = Button(ai_settings, screen, 'Play')
+    #Название окна
+    pygame.display.set_caption("Alien invasion")
+    #Создание класов для статистики
+    
+    if (not os.path.exists(filename)): open(filename, "w+").write('0')
+    stats = GameStats(ai_settings, filename)
+    sb = Scoreboard(ai_settings, screen, stats)
+    #Создание корабля, группы пуль ипришельцев
+    ship = Ship(screen, ai_settings)
+    bullets = Group()
+    aliens = Group()
+    gf.create_fleet(ai_settings, screen, ship, aliens)
+    
+    while True:
+        #Цикл по поиску событий и реагирования на них
+        gf.check_events(ship, ai_settings, bullets, screen, stats, play_button, 
+        aliens, sb, filename)
+        if stats.game_active:
+            ship.update()
+            gf.update_bullets(ai_settings, screen, ship, aliens, bullets, 
+               stats, sb)
+            gf.update_aliens(stats, ai_settings, screen, ship, aliens, bullets,
+               sb)
+        gf.update_screen(screen, ai_settings, ship, bullets, aliens, stats, 
+           play_button, sb)
+
+run_game()
+    
